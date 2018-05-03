822f13890ce46e3265774552b072fa7a54707303
JaviFigueroaRUM05 <31051758+JaviFigueroaRUM05@users.noreply.github.com>
2018-05-02 23:32:07 -0400

    Uploaded Lab 10

---
+++ Lab10/src/testerClasses/PriorityQueueTester3.java
@@ -0,0 +1,33 @@
+package testerClasses;
+
+import java.util.Comparator;
+
+import priorityQueue.UnsortedListPriorityQueue;
+import priorityQueueInterfaces.PriorityQueue;
+
+public class PriorityQueueTester3 {
+	
+	public class StringComparator1 implements Comparator<String> { 
+	     public int compare(String s1, String s2) { 
+	         return s2.compareTo(s1); 
+	     } 
+	}
+
+	String[] arr = {"barrio", "pepe", "julia", "maria", "oliva", "meme", "parada", "baile", "enjendro", 
+	         "vagabundo", "nota", "tienda", "zapato", "caballo", "cafe", "diodo", "multiplica"};
+
+	
+	PriorityQueue<String, String> pq;
+	
+	pq = new UnsortedListPriorityQueue<String, String>(new StringComparator1());    // 1
+	//pq = new SortedListPriorityQueue<String, String>(new StringComparator1());    // 2
+	//pq = new HeapListPriorityQueue<String, String>(new StringComparator1());      // 3
+	//pq = new HeapPriorityQueue<String, String>(new StringComparator1());          // 4
+	
+	TesterUtils.displayArray("Content of the array before ", arr); 
+	TesterUtils.what(arr, pq); 
+	TesterUtils.displayArray("Content of the array after ", arr); 
+}
+
+
+
