1edd461a23fb7d97f1be1d02e35ab581323b7369
JaviFigueroaRUM05 <31051758+JaviFigueroaRUM05@users.noreply.github.com>
2018-05-03 00:02:34 -0400

    Update

--- Lab10/src/testerClasses/PriorityQueueTester3.java
+++ Lab10/src/testerClasses/PriorityQueueTester3.java
@@ -12,22 +12,23 @@ public class PriorityQueueTester3 {
 	         return s2.compareTo(s1); 
 	     } 
 	}
-
-	String[] arr = {"barrio", "pepe", "julia", "maria", "oliva", "meme", "parada", "baile", "enjendro", 
+        public void main(){
+	     String[] arr = {"barrio", "pepe", "julia", "maria", "oliva", "meme", "parada", "baile", "enjendro", 
 	         "vagabundo", "nota", "tienda", "zapato", "caballo", "cafe", "diodo", "multiplica"};
 
 	
-	PriorityQueue<String, String> pq;
+	     PriorityQueue<String, String> pq;
 	
-	pq = new UnsortedListPriorityQueue<String, String>(new StringComparator1());    // 1
-	//pq = new SortedListPriorityQueue<String, String>(new StringComparator1());    // 2
-	//pq = new HeapListPriorityQueue<String, String>(new StringComparator1());      // 3
-	//pq = new HeapPriorityQueue<String, String>(new StringComparator1());          // 4
+	     pq = new UnsortedListPriorityQueue<String, String>(new StringComparator1());    // 1
+       	     //pq = new SortedListPriorityQueue<String, String>(new StringComparator1());    // 2
+	     //pq = new HeapListPriorityQueue<String, String>(new StringComparator1());      // 3
+	     //pq = new HeapPriorityQueue<String, String>(new StringComparator1());          // 4
 	
-	TesterUtils.displayArray("Content of the array before ", arr); 
-	TesterUtils.what(arr, pq); 
-	TesterUtils.displayArray("Content of the array after ", arr); 
+	     TesterUtils.displayArray("Content of the array before ", arr); 
+	     TesterUtils.what(arr, pq); 
+	     TesterUtils.displayArray("Content of the array after ", arr); 
+
+	}
+
+
 }
-
-
-
